b0a20c0f72cfc5665727e0548fafb759de19340d
giangnh264 <giangnh.264@gmail.com>
2016-10-14 22:08:12 +0700

    D

--- frontend/views/layouts/_top_nav.php
+++ frontend/views/layouts/_top_nav.php
@@ -8,19 +8,14 @@
             <ul class="nav navbar-nav navbar-right">
                 <li class="">
                     <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                        John Doe
+                        Demo
                         <span class=" fa fa-angle-down"></span>
                     </a>
                     <ul class="dropdown-menu dropdown-usermenu pull-right">
-                        <li><a href="javascript:;"> Profile</a></li>
-                        <li>
-                            <a href="javascript:;">
-                                <span class="badge bg-red pull-right">50%</span>
-                                <span>Settings</span>
-                            </a>
-                        </li>
-                        <li><a href="javascript:;">Help</a></li>
-                        <li><a href="login.html"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
+                        <li><a href="javascript:;">Thông tin</a></li>
+                        <li><a href="javascript:;">Đổi mật khẩu</a></li>
+                        <li><a href="javascript:;">Trợ giúp</a></li>
+                        <li><a href="login.html"><i class="fa fa-sign-out pull-right"></i>Thoát</a></li>
                     </ul>
                 </li>
                 <li class="">
@@ -29,15 +24,11 @@
                         <span class=" fa fa-angle-down"></span>
                     </a>
                     <ul class="dropdown-menu dropdown-usermenu pull-right">
-                        <li><a href="javascript:;"> Profile</a></li>
-                        <li>
-                            <a href="javascript:;">
-                                <span class="badge bg-red pull-right">50%</span>
-                                <span>Settings</span>
-                            </a>
-                        </li>
-                        <li><a href="javascript:;">Help</a></li>
-                        <li><a href="login.html"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
+                        <li><a href="javascript:;">Giám sát</a></li>
+                        <li><a href="javascript:;">Báo cáo</a></li>
+                        <li><a href="javascript:;">Quản trị</a></li>
+                        <li><a href="javascript:;">Quản trị</a></li>
+                        <li><a href="javascript:;">Quản trị</a></li>
                     </ul>
                 </li>
                 <li class="">
@@ -46,15 +37,7 @@
                         <span class=" fa fa-angle-down"></span>
                     </a>
                     <ul class="dropdown-menu dropdown-usermenu pull-right">
-                        <li><a href="javascript:;"> Profile</a></li>
-                        <li>
-                            <a href="javascript:;">
-                                <span class="badge bg-red pull-right">50%</span>
-                                <span>Settings</span>
-                            </a>
-                        </li>
-                        <li><a href="javascript:;">Help</a></li>
-                        <li><a href="login.html"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
+                        <li><a href="javascript:;">Về chúng tôi</a></li>
                     </ul>
                 </li>
             </ul>
