6211d1cbbbece591c098c74e4e47603eeb8d69f4
giangnh264 <giangnh.264@gmail.com>
2016-10-16 10:31:53 +0700

    cm code

--- frontend/views/layouts/_menu_left.php
+++ frontend/views/layouts/_menu_left.php
@@ -4,38 +4,38 @@
             <ul class="nav side-menu">
                 <li><a><i class="fa fa-home"></i>Giám sát<span class="fa fa-chevron-down"></span></a>
                     <ul class="nav child_menu">
-                        <li><a href="index.html">Xem cam</a></li>
-                        <li><a href="index2.html">Xem lại</a></li>
-                        <li><a href="index3.html">Kiểm soát</a></li>
+                        <li><a href="#">Xem cam</a></li>
+                        <li><a href="#">Xem lại</a></li>
+                        <li><a href="#">Kiểm soát</a></li>
                     </ul>
                 </li>
                 <li><a><i class="fa fa-edit"></i>Báo cáo<span class="fa fa-chevron-down"></span></a>
                     <ul class="nav child_menu">
-                        <li><a href="form.html">Báo cáo chấm công</a></li>
-                        <li><a href="form_advanced.html">Dữ liệu gốc</a></li>
-                        <li><a href="form_validation.html">Báo cáo video</a></li>
-                        <li><a href="form_wizards.html">Báo cáo ảnh</a></li>
+                        <li><a href="#">Báo cáo chấm công</a></li>
+                        <li><a href="#">Dữ liệu gốc</a></li>
+                        <li><a href="#">Báo cáo video</a></li>
+                        <li><a href="#">Báo cáo ảnh</a></li>
                     </ul>
                 </li>
                 <li><a><i class="fa fa-desktop"></i>Quản trị<span class="fa fa-chevron-down"></span></a>
                     <ul class="nav child_menu">
-                        <li><a href="general_elements.html">Cấu hình máy châm công</a></li>
+                        <li><a href="#">Cấu hình máy châm công</a></li>
                         <li><a href="<?=\yii\helpers\Url::base()?>/camera/index">Cấu hình Camera</a></li>
-                        <li><a href="typography.html">Cấu hình trên máy tính</a></li>
-                        <li><a href="icons.html">Nhân viên</a></li>
-                        <li><a href="glyphicons.html">Tài khoản</a></li>
-                        <li><a href="widgets.html">Đổi mật khẩu</a></li>
-                        <li><a href="invoice.html">Phân quyền</a></li>
-                        <li><a href="inbox.html">Công ty</a></li>
+                        <li><a href="#">Cấu hình trên máy tính</a></li>
+                        <li><a href="#">Nhân viên</a></li>
+                        <li><a href="#">Tài khoản</a></li>
+                        <li><a href="#">Đổi mật khẩu</a></li>
+                        <li><a href="#">Phân quyền</a></li>
+                        <li><a href="#">Công ty</a></li>
                     </ul>
                 </li>
                 <li><a><i class="fa fa-table"></i>Hướng dẫn<span class="fa fa-chevron-down"></span></a>
                     <ul class="nav child_menu">
-                        <li><a href="tables.html">Lưu dữ liệu trên máy tính</a></li>
-                        <li><a href="tables_dynamic.html">Cài teamview</a></li>
-                        <li><a href="tables_dynamic.html">Cấu hình cam</a></li>
-                        <li><a href="tables_dynamic.html">Cấu hình máy chấm công</a></li>
-                        <li><a href="tables_dynamic.html">Mở port Wifi</a></li>
+                        <li><a href="#">Lưu dữ liệu trên máy tính</a></li>
+                        <li><a href="#">Cài teamview</a></li>
+                        <li><a href="#">Cấu hình cam</a></li>
+                        <li><a href="#">Cấu hình máy chấm công</a></li>
+                        <li><a href="#">Mở port Wifi</a></li>
                     </ul>
                 </li>
             </ul>
