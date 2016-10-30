dcd001607dbe6f77af7b9a30e1a7c3fe4564e714
thongnv10 <thongnv1010@gmail.com>
2016-10-30 10:49:06 +0700

    update runtime

--- common/config/main.php
+++ common/config/main.php
@@ -1,6 +1,8 @@
 <?php
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+	 'basePath' => dirname(__DIR__),
+    'runtimePath' => dirname(__DIR__) . '/../../runtime',
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
