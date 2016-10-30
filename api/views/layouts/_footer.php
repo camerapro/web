8616f4618f4b835f9f91f870683cbc68f5ad5ada
thongnv10 <thongnv1010@gmail.com>
2016-10-30 10:34:19 +0700

    update remove asss

--- api/views/layouts/_footer.php
+++
@@ -1,6 +0,0 @@
-<footer>
-    <div class="pull-right">
-        Thiết bị an ninh - <a href="http://thietbianninh.com">http://thietbianninh.com</a>
-    </div>
-    <div class="clearfix"></div>
-</footer>
