541c7fdb82f33840ecb7b44b837688b071d71982
thongnv10 <thongnv1010@gmail.com>
2016-10-30 17:24:27 +0700

    update api Camera

--- common/models/Camera.php
+++ common/models/Camera.php
@@ -28,18 +28,18 @@ use common\models\_base\RelationsCamUserBase;
 class Camera extends CameraBase
 {
     public static  function add($params){
-        $tat = new self;
-        $tat->attributes = $params;
-        $rt = $tat->save(false);
+        $cam = new self;
+        $cam->attributes = $params;
+        $rt = $cam->save(false);
         if($rt){
             $camera_user = new RelationsCamUserBase();
-            $camera_user->cam_id = $tat->id;
+            $camera_user->cam_id = $cam->id;
             $camera_user->created_by_id = Yii::$app->user->identity->id;
             $camera_user->user_id = Yii::$app->user->identity->id;
             $camera_user->created_by_name = Yii::$app->user->identity->username;
             $camera_user->created_time = date('Y-m-d H:i:s');
             $camera_user->save(false);
         }
-        return $tat;
+        return $cam;
     }
 }
