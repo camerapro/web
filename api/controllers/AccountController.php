a3ae57a938b84687b08b1b899b95b88851b91e7e
thongnv10 <thongnv1010@gmail.com>
2016-10-30 10:42:48 +0700

    udpate api

---
+++ api/controllers/AccountController.php
@@ -0,0 +1,109 @@
+<?php
+namespace api\controllers;
+
+use Yii;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+use common\models\LoginForm;
+/**
+ * Site controller
+ */
+class AccountController extends Controller
+{
+	public $enableCsrfValidation = false;
+	private $api_key ='43S4342@342Asfd';
+	public $layout =false;
+	public function init()
+    {
+        \Yii::$app->response->format = 'json';
+    }
+    /**
+     * @inheritdoc
+     */
+    /*public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['logout', 'signup'],
+                'rules' => [
+                    [
+                        'actions' => ['signup'],
+                        'allow' => true,
+                        'roles' => ['?'],
+                    ],
+                    [
+                        'actions' => ['logout'],
+                        'allow' => false,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }*/
+
+    /**
+     * @inheritdoc
+     */
+    public function actions()
+    {
+        return [
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+    /**
+     * Displays homepage.
+     *
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+       die("HELLO API");
+    }
+
+    /**
+     * Logs in a user.
+     *
+     * @return mixed
+     */
+    public function actionLogin()
+    {
+        $this->layout = 'login/main.php';
+        if (!Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+        $model = new LoginForm();
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            return $this->goHome();
+        } else {
+            return $this->render('login', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Logs out the current user.
+     *
+     * @return mixed
+     */
+    public function actionLogout()
+    {
+        Yii::$app->user->logout();
+
+        return $this->goHome();
+    }
+
+
+}
---
+++ api/controllers/RecorderController.php
@@ -0,0 +1,91 @@
+<?php
+namespace api\controllers;
+
+use Yii;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+use common\models\LoginForm;
+/**
+ * Site controller
+ */
+class RecorderController extends Controller
+{
+	public $enableCsrfValidation = false;
+	private $api_key ='43S4342@342Asfd';
+	public $layout =false;
+	public function init()
+    {
+        \Yii::$app->response->format = 'json';
+    }
+    public function actionGet()
+    {
+        $cam_info = [];
+        $message = '';
+        $cam_id = isset(Yii::$app->request->get()['id']) ? Yii::$app->request->get()['id'] : '';
+        if(!empty($cam_id)){
+            $cam_info = Camera::getOneCamById($cam_id);
+            if(!$cam_info){
+                return ['error_code'=>1,'message'=>'No permistion'];
+            }
+        }else{
+            $cams = \frontend\models\Camera::getListCam();
+            if(!empty($cams)){
+                $cam_info = $cams;
+				return ['error_code'=>0,'message'=>'Success','data'=>$cam_info];
+            }
+            if(empty($cam_info)){
+                return ['error_code'=>401,'message'=>'Data empty','data'=>[]];
+            }
+        }
+    }
+    public function actionAdd()
+    {
+          if($data = Yii::$app->request->post())
+            {
+            $camera = new Camera();
+            $camera->name = $data['name'];
+            $camera->ip_address = $data['ip'];
+            $camera->protocol = $data['protocol'];
+            $camera->port = $data['port'];
+            $camera->channel = $data['channel'];
+            $camera->encoder_username = $data['username'];
+            $camera->encoder_password = $data['password'];
+            $camera->created_time = date('Y-m-d H:i:s');
+            $camera->updated_time = date('Y-m-d H:i:s');
+            $camera->user_id = Yii::$app->user->identity->id;
+			
+            if($data['protocol'] == 'http')
+                $camera->streaming_url = $data['ip'];
+            elseif ($data['protocol'] == 'rtsp')
+                $camera->streaming_url = 'rtsp://' .$data['ip']. ':' . $data['port'] . '/user=' . $data['username'] . '&password='.$data['password'] . '&channel=' . $data['channel'] . '&stream=1.sdp';
+            $save = $camera->save(false);
+            if($save){
+               /* $camera_user = new RelationsCamUser();
+                $camera_user->cam_id = $camera->id;
+                $camera_user->created_by_id = Yii::$app->user->identity->id;
+                $camera_user->user_id = Yii::$app->user->identity->id;
+                $camera_user->created_by_name = Yii::$app->user->identity->username;
+                $camera_user->created_time = date('Y-m-d H:i:s');
+                $camera_user->save();*/
+                $return = array(
+                    'error_code'=>0,
+                    'message'=>'Success'
+                );
+            }else{
+                $return = array(
+                    'error_code'=>1,
+                    'message'=>'Add fail'
+                );
+            }
+		}else{
+            $return = array(
+                'error_code'=>1,
+                'message'=>'Method not supported!'
+            );
+        }
+        echo json_encode($return);
+        exit;
+        
+    }
+}
