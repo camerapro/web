6abfb8343468883a22ca560ed25147850ee16402
thongnv10 <thongnv1010@gmail.com>
2016-11-01 00:59:15 +0700

    Update staff

--- common/models/Timekeeping.php
+++ common/models/Timekeeping.php
@@ -20,5 +20,19 @@ use Yii;
  */
 class Timekeeping extends TimekeepingBase
 {
-
+    public static  function add($params){
+        $tat = new self;
+        $tat->attributes = $params;
+        return $tat->save(false);
+    }
+    public static  function getByUserId($userId=0){
+        $return = self::find()->where(['created_by'=>$userId])->all();
+        $rt = array();
+        foreach ($return as $value)
+        {
+            $value->image = 'http://api.thietbianninh.com/kute.jpg';
+            $rt[] = $value;
+        }
+        return $rt;
+    }
 }
