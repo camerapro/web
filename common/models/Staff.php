94526efeef8581fdacd4d1e59f94489c6d982bc8
thongnv10 <thongnv1010@gmail.com>
2016-10-31 23:53:47 +0700

    Update staff

--- common/models/Staff.php
+++ common/models/Staff.php
@@ -13,4 +13,7 @@ class Staff extends StaffBase
         $tat->attributes = $params;
         return $tat->save(false);
     }
+    public static  function getStaffByUserId($userId=0){
+        return self::find()->where(['created_by'=>$userId])->asArray()->all();
+    }
 }
