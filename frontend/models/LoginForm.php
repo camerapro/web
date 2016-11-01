decc6e264d3051f11b65a29a011d8bb48bceb998
koigiang <giangnh.264@gmail.com>
2016-11-02 00:32:52 +0700

    chinh sua lai ten model

--- frontend/models/LoginForm.php
+++ frontend/models/LoginForm.php
@@ -63,7 +63,7 @@ class LoginForm extends Model
      */
     public function login()
     {
-        print_r(static::findOne(['username' => $this->username, 'status' => self::STATUS_ACTIVE]));exit;
+        print_r(static::findOne(['username' => $this->username, 'status' => 1]));exit;
         if ($this->validate()) {
             return Yii::$app->user->login($this->getUser(), 3600 * 24 * 30);
         } else {
