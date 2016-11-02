5c231e804f72a083c829540aa6f55313cd6c3bb3
koigiang <giangnh.264@gmail.com>
2016-11-03 00:01:45 +0700

    tesst

--- frontend/views/camera/index.php
+++ frontend/views/camera/index.php
@@ -51,7 +51,7 @@ $this->params['breadcrumbs'][] = $this->title;
                     <?= $cam->channel;?>
                 </td>
                 <td>
-                    <?= $cam->encoder_username;?>
+                    <?= \frontend\models\RelationsCamUser::findOne(['cam_id'=>$cam->id, 'owner'=>1])->created_by_name;?>
                 </td>
                 <td>
                     <a href="<?=  Url::to(['site/index', 'id' => $cam->id])?>"><?= \common\components\Common::getLinkStream($cam->id);?></a>
