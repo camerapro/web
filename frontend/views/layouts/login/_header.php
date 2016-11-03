6c0abccc8290f620508ca703c58cb2b8e1639f5f
koigiang <giangnh.264@gmail.com>
2016-11-03 21:31:20 +0700

    debug

--- frontend/views/layouts/login/_header.php
+++ frontend/views/layouts/login/_header.php
@@ -15,7 +15,7 @@
     <link href="/css/login.css" rel="stylesheet">
     <link href="/css/define.css" rel="stylesheet">
 <!--    <script src="https://colorlib.com/polygon/vendors/jquery/dist/jquery.min.js"></script>-->
-    <script type="text/javascript" src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
 
     <script src="/js/smartwizard.js"></script>
 
