2067f4deea0a88ffa44a6136d165ae2e961b26e7
koigiang <giangnh.264@gmail.com>
2016-11-03 21:42:54 +0700

    debug

--- frontend/views/layouts/_header.php
+++ frontend/views/layouts/_header.php
@@ -4,6 +4,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="cache-control" content="no-cache">
+    <meta http-equiv="expires" content="-1">
+    <meta http-equiv="pragma" content="no-cache">
     <title>Thiết bị An Ninh | Astech Việt Nam</title>
     <link href="/web/images/favicon.ico" rel="shortcut icon" type="image/x-icon" />
     <!-- Bootstrap -->
