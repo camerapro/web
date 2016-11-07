f1db27282fddebbae778c8e1f6b3e80ce3087cf4
thongnv10 <thongnv1010@gmail.com>
2016-11-07 22:21:37 +0700

    dd

--- api/controllers/AccountController.php
+++ api/controllers/AccountController.php
@@ -73,7 +73,7 @@ class AccountController extends Controller
        die("HELLO API");
     }
 
-     public function actionLogin()
+    public function actionLogin()
     {
         if (!Yii::$app->user->isGuest) {
 			
@@ -92,6 +92,35 @@ class AccountController extends Controller
 			if ($model->load(['LoginForm' => Yii::$app->request->post()]) && $model->login()) {
 				$permission_group_id = Yii::$app->user->identity->permission_group_id;
 				//$permission = \common\models\Permission::getListPermissionById(Yii::$app->user->identity->id);
+				$permission = null;
+				return ['error_code'=>0,'message'=>'Logined','data'=>['userid'=>Yii::$app->user->identity->id,'username'=>Yii::$app->user->identity->username,'permission_group_id'=>$permission_group_id,'permission'=>$permission]];
+			} else {
+				return ['error_code'=>1,'message'=>'Login fail'];
+			}
+		}
+	
+		
+        
+    }
+	public function actionLogintest()
+    {
+        if (!Yii::$app->user->isGuest) {
+			
+			$permission_group_id = Yii::$app->user->identity->permission_group_id;
+			$permission = \common\models\Permission::getListPermissionById(Yii::$app->user->identity->id);
+			$permission = null;
+			return ['error_code'=>0,'message'=>'Logined','data'=>['userid'=>Yii::$app->user->identity->id,'username'=>Yii::$app->user->identity->username,'permission_group_id'=>$permission_group_id,'permission'=>$permission]];
+			
+        }
+		if($post = Yii::$app->request->post()){
+			
+			if(!$this->validateToken($post)){
+				return ['error_code'=>1,'message'=>'Validate token fail'];
+			}
+			$model = new LoginForm();
+			if ($model->load(['LoginForm' => Yii::$app->request->post()]) && $model->login()) {
+				$permission_group_id = Yii::$app->user->identity->permission_group_id;
+				$permission = \common\models\Permission::getListPermissionById(Yii::$app->user->identity->id);
 				$permission = null;
 				return ['error_code'=>0,'message'=>'Logined','data'=>['userid'=>Yii::$app->user->identity->id,'username'=>Yii::$app->user->identity->username,'permission_group_id'=>$permission_group_id,'permission'=>$permission]];
 			} else {
