11786faaa0b5234620cbf5aa61a1f995f1f66926
thongnv10 <thongnv1010@gmail.com>
2016-11-07 22:03:08 +0700

    update Permission.php

---
+++ common/models/Permission.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace commond\models;
+
+use common\models\_base\PermissionBase;
+use Yii;
+
+
+class Permission extends PermissionBase
+{
+    
+       public static function getListPermissionById($user_id){
+        $query = self::find()
+            -> leftJoin('relations_user_permission_group', 'relations_user_permission_group.permission_group_id = permission.permission_group_id')
+            -> leftJoin('relations_user_permission_group', 'relations_user_permission_group.permission_group_id = permission.permission_group_id')
+            ->where(['=', 'relations_user_permission_group.user_id', $user_id])
+            ->all();
+        return $query;
+    }
+
+    public function getAll(){
+        $data = [];
+        $list = self::find()
+            ->where(['=', 'parent_id', 0])
+            ->asArray()
+            ->all();
+        foreach ($list as $item){
+            $data[$item['id']]  = $item;
+            $data[$item['id']]['child'] =  self::getChildPer($item['id']);
+        }
+        return $data;
+    }
+
+    public function getChildPer($parrent_id){
+        return self::find()
+            ->where(['=', 'parent_id', $parrent_id])
+            ->asArray()
+            ->all();
+    }
+
+    public function getPermissionName($pemission_ids){
+        $list_permission_by_group = explode(',', $pemission_ids);
+        $name = [];
+        foreach ($list_permission_by_group as $item){
+            $name[] =   self::findOne($item)->name;
+        }
+        $name = implode(', ', $name);
+        return $name;
+    }
+}
