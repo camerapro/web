5e7858ff2da8340f11dae6cededd678c9a645bd7
koigiang <giangnh.264@gmail.com>
2016-11-07 23:09:47 +0700

    edit conflic

--- common/models/_base/CameraBase.php
+++ common/models/_base/CameraBase.php
@@ -30,7 +30,6 @@ use Yii;
  * @property integer $agency_id
  * @property string $encoder_model
  * @property integer $quality
- * @property integer $recorder_id
  * @property string $activation_time
  */
 class CameraBase extends \yii\db\ActiveRecord
@@ -49,13 +48,8 @@ class CameraBase extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-<<<<<<< HEAD
             [['recorder_id', 'category_id', 'encoder_port', 'port', 'order', 'status', 'thumb_version', 'user_id', 'agency_id', 'quality'], 'integer'],
             [['created_time', 'updated_time', 'activation_time'], 'safe'],
-=======
-            [['category_id', 'encoder_port', 'port', 'order', 'status', 'thumb_version', 'user_id', 'agency_id', 'quality','recorder_id'], 'integer'],
-            [['created_time', 'updated_time','activation_time'], 'safe'],
->>>>>>> 05265ffb60f9b245628508fcc6ee3d178a42f8b4
             [['name', 'encoder_name', 'streaming_url', 'ip_address', 'encoder_username', 'encoder_password', 'channel', 'params', 'encoder_model'], 'string', 'max' => 255],
             [['protocol'], 'string', 'max' => 15],
         ];
@@ -90,11 +84,8 @@ class CameraBase extends \yii\db\ActiveRecord
             'agency_id' => 'Agency ID',
             'encoder_model' => 'Encoder Model',
             'quality' => 'Quality',
-<<<<<<< HEAD
             'activation_time' => 'Activation Time',
-=======
             'recorder_id' => 'recorder ID',
->>>>>>> 05265ffb60f9b245628508fcc6ee3d178a42f8b4
         ];
     }
 }
