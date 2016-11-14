eaca25d0a8729ec20403500b513a5bc0e2d69bb1
thongnv10 <thongnv1010@gmail.com>
2016-11-15 02:00:55 +0700

    sdd

--- common/components/Common.php
+++ common/components/Common.php
@@ -5,6 +5,7 @@ namespace common\components;
 use common\models\_base\CameraBase;
 use common\models\Recorder;
 use frontend\models\Camera;
+use Yii;
 
 class Common
 {
@@ -17,6 +18,11 @@ class Common
         }
         $pattern = "/^(089|086|088|090|091|092|093|094|095|096|097|098|099|0121|0122|0123|0124|0125|0126|0127|0128|0129|0121|0120||0161|0162|0163|0164|0165|0166|0167|0168|0169)([0-9]{7})$/";
         return preg_match($pattern,$phone);
+    }
+    public static function getImage($data,$type ='staff')
+    {
+        $company_id = isset($data->company_id)?$data->company_id :'';
+        return Yii::$app->params['images'][$type]['url'].'/'.  $company_id.'/'.$data->id;
     }
     public static function uploadFile($file, $target, $object, $extension, $options = array(), $endcode = false)
     {
