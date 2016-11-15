8095545cadabadfef2d3bd0bf3fb95853eb016fa
thongnv10 <thongnv1010@gmail.com>
2016-11-16 02:03:08 +0700

    xdd

--- frontend/views/staff/index.php
+++ frontend/views/staff/index.php
@@ -60,6 +60,21 @@ $this->params['breadcrumbs'][] = $this->title;
                         Html::img(\common\components\Common::getImage($data,'staff'),['width'=>'100%', 'title' => $data->{'name'}]).'</a>' : null;
                 }
             ],
+			 ['attribute' => 'company_id',
+                'format' => 'raw',
+                'filter' =>  yii\helpers\ArrayHelper::map(\frontend\models\CompanyFrontend::findAll(['status' => 1]), 'id', 'name'),
+                'options' => ['width' => '90px'],
+                'value' => function ($data) {
+                   $company = \frontend\models\CompanyFrontend::find()->where(['id' => $data->company_id])->one();
+                    if (!empty($company)) {
+                        return $company->name;
+                    }
+                },
+                'headerOptions' => ['style' => 'text-align: center; vertical-align: middle;'],
+                'contentOptions' => ['style' => 'text-align: center; vertical-align: middle;']
+            ],
+		
+			
             // 'created_time',
             // 'created_by',
             // 'updated_time',
