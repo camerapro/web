8b47fcdac1b993a33c534d04920d9267bfdee340
thongnv <thongnv@vega.com.vn>
2016-11-15 10:41:15 +0700

    dđ

--- common/models/Camera.php
+++ common/models/Camera.php
@@ -37,7 +37,7 @@ class Camera extends CameraBase
                 ->select(['id','name','channel','params','camera.status','camera.recorder_id','streaming_url','relations_cam_user.user_id','quality','camera.activation_time'])
                 ->leftJoin('relations_cam_user', 'relations_cam_user.cam_id=camera.id')
                 ->where(['recorder_id'=>$recorder_id])
-				->orWhere(['camera.status'=>0,'camera.status'=>1]);
+				->orWhere(['camera.status'=>0,'camera.status'=>1])
                 ->andWhere(['=', 'relations_cam_user.user_id', $user_id])
                 ->all();
             foreach($rec as $camera ){
