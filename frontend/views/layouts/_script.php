c0bb69eba7b33547e5ab2cf243df183f9946fb75
koigiang <giangnh.264@gmail.com>
2016-11-16 23:18:47 +0700

    cm code

--- frontend/views/layouts/_script.php
+++ frontend/views/layouts/_script.php
@@ -1,5 +1,8 @@
-<script type="text/javascript" src="https://colorlib.com/polygon/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="https://colorlib.com/polygon/build/js/custom.min.js"></script>
+<!--<script type="text/javascript" src="https://colorlib.com/polygon/vendors/bootstrap/dist/js/bootstrap.min.js"></script>-->
+<script src="/js/bootstrap.min.js"></script>
+<script src="/js/custom.min.js"></script>
+
+<!--<script type="text/javascript" src="https://colorlib.com/polygon/build/js/custom.min.js"></script>-->
 
 <script src="https://colorlib.com/polygon/vendors/fastclick/lib/fastclick.js"></script>
 <script src="https://colorlib.com/polygon/vendors/nprogress/nprogress.js"></script>
