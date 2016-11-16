652f14e4a7219fd495efd2cd694db73bfd4c75e4
thongnv10 <thongnv1010@gmail.com>
2016-11-16 23:56:01 +0700

    update Image form

--- frontend/views/staff/_form.php
+++ frontend/views/staff/_form.php
@@ -82,8 +82,8 @@ use yii\bootstrap\ActiveForm;
             <div class="col-xs-4">
                 <div class="row">
                     <div class=" col-md-push-4" >
-                        <img src="http://static.thietbianninh.com/staff/thumb.png" width="180"></img>
-                        <?= $form->field($model, 'image')->fileInput()->label(false) ?>
+                        <img src="<?php echo \common\components\Common::getImage($model,'staff')?>"  onerror="this.src='<?php echo Yii::$app->params['images']['staff']['url'].'/thumb.png';?>'" width="180"></img>
+                        <?= $form->field($model, 'imageFile')->fileInput()->label(false) ?>
                     </div>
                 </div>
             </div>
