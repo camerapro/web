34e00c0ebee298c058220a778ae87699d4bf5330
koigiang <giangnh.264@gmail.com>
2016-11-16 23:15:26 +0700

    cm code

--- frontend/views/layouts/login/_header.php
+++ frontend/views/layouts/login/_header.php
@@ -1,37 +1,29 @@
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <!-- Meta, title, CSS, favicons, etc. -->
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<!-- Meta, title, CSS, favicons, etc. -->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <meta http-equiv="cache-control" content="max-age=0" />
-    <meta http-equiv="cache-control" content="no-cache" />
-    <meta http-equiv="expires" content="0" />
-    <meta http-equiv="pragma" content="no-cache" />
+<meta http-equiv="cache-control" content="max-age=0" />
+<meta http-equiv="cache-control" content="no-cache" />
+<meta http-equiv="expires" content="0" />
+<meta http-equiv="pragma" content="no-cache" />
 
-    <title>Đăng nhập - Thiết bị An Ninh | Astech Việt Nam</title>
+<title>Đăng nhập - Thiết bị An Ninh | Astech Việt Nam</title>
 
-    <!-- Bootstrap -->
-<!--    <link href="https://colorlib.com/polygon/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">-->
-<!--    <!-- Font Awesome -->-->
-<!--    <link href="https://colorlib.com/polygon/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">-->
-<!--    <!-- Custom Theme Style -->-->
-<!--    <link href="https://colorlib.com/polygon/build/css/custom.min.css" rel="stylesheet">-->
-    <link href="/css/style.css" rel="stylesheet">
-    <link href="/css/login.css" rel="stylesheet">
-    <link href="/css/define.css" rel="stylesheet">
+<!-- Bootstrap -->
+<link href="/css/bootstrap.min.css" rel="stylesheet">
+<!-- Font Awesome -->
+<link href="/css/font-awesome.min.css" rel="stylesheet">
+<!-- Custom Theme Style -->
 
+<link href="/css/custom.min.css" rel="stylesheet">
+<link href="/css/style.css" rel="stylesheet">
+<link href="/css/login.css" rel="stylesheet">
+<link href="/css/define.css" rel="stylesheet">
+<!--    <script src="https://colorlib.com/polygon/vendors/jquery/dist/jquery.min.js"></script>-->
 
+<script src="/js/jquery-2.2.4.min.js"></script>
+<script src="/js/smartwizard.js"></script>
 
-
-    <link href="/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/css/font-awesome.min.css" rel="stylesheet">
-    <link href="/css/custom.min.css" rel="stylesheet">
-
-
-
-
-<!--    <script type="text/javascript" src="https://code.jquery.com/jquery-2.2.4.min.js"></script>-->
-
-    <script src="/js/smartwizard.js"></script>
-    <script src="/js/jquery-2.2.4.min.js"></script>
+<!--    <script src="/js/common.js"></script>-->
