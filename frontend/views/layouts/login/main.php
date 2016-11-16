048d06705ce1744f2bca94f761476f743e2203e3
koigiang <giangnh.264@gmail.com>
2016-11-16 23:19:53 +0700

    cm code

--- frontend/views/layouts/login/main.php
+++ frontend/views/layouts/login/main.php
@@ -6,7 +6,7 @@
 
 <body class="login">
 <?= $content ?>
-<script src="https://colorlib.com/polygon/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
-<script src="https://colorlib.com/polygon/build/js/custom.min.js"></script>
+<script src="/js/bootstrap.min.js"></script>
+<script src="/js/custom.min.js"></script>
 </body>
 </html>
