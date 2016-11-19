e0d7fccf368ac43cee0ca3d89ab08de05e1350b0
thongnv10 <thongnv1010@gmail.com>
2016-11-19 12:49:54 +0700

    update

--- frontend/views/timekeeping/index.php
+++ frontend/views/timekeeping/index.php
@@ -42,7 +42,7 @@ $this->params['breadcrumbs'][] = $this->title;
 	
 	</div>
 	<span class="btn-del-confirm"><img src="/images/btn-del-confirm.png" width="65"></span>
-	<span class="btn-del-confirm"><img src="/images/btn-export-file" width="65"></span>
+	<span class="btn-del-confirm"><img src="/images/btn-export-file.png" width="65"></span>
 		
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
