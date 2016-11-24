7f688d559810f28f67a9c3cb4cd09a0258958e8f
thongnv <thongnv@vega.com.vn>
2016-11-24 17:58:25 +0700

    Update api channel

--- common/models/Camera.php
+++ common/models/Camera.php
@@ -77,7 +77,8 @@ class Camera extends CameraBase
         $port = isset($recorder->port_stream)?$recorder->port_stream:'554';
         $username = isset($recorder->username)?$recorder->username:'';
         $password = isset($recorder->password)?$recorder->password:'';
-        $channel = isset($camera->channel)?$camera->channel:'';
+        $channel = (int)isset($camera->channel)?$camera->channel:'';
+		$channel =  $channel +1;
         $quality = 1;
         $stream = $protocol."://".$ip.":".$port."/?user=".$username."&password=".$password.'&channel='.$channel.'&stream='.$quality .'.sdp';
         return $stream ;
