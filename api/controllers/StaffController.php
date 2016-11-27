7f70ed8c36100be04baa25c920b543b9a267d5d4
thongnv10 <thongnv1010@gmail.com>
2016-11-27 16:06:01 +0700

    update log

--- api/controllers/StaffController.php
+++ api/controllers/StaffController.php
@@ -17,10 +17,11 @@ class StaffController extends Controller
     public $enableCsrfValidation = false;
     private $api_key = '43S4342@342Asfd';
     public $layout = false;
-	public $logger;
+    public $logger;
+
     public function init()
     {
-		$this->logger = new KLogger('api_' . date('Ymd'), KLogger::INFO);
+        $this->logger = new KLogger('api_' . date('Ymd'), KLogger::INFO);
         \Yii::$app->response->format = 'json';
     }
 
@@ -37,35 +38,35 @@ class StaffController extends Controller
 
     public function actionAdd()
     {
-		$this->logger->LogInfo("actionAdd staff data  :" .json_encode(Yii::$app->request->post()));
-		
+        $this->logger->LogInfo("actionAdd staff data  :" . json_encode(Yii::$app->request->post()));
+
         if (Yii::$app->user->isGuest) {
             return ['error_code' => 1, 'message' => 'Not login'];
         }
         if ($data = Yii::$app->request->post()) {
 
-			$staff =  new Staff();
-            $staff->name= isset($data['name']) ? $data['name'] : '';
-			$staff->phone= isset($data['phone']) ? $data['phone'] : '';
-			$staff->email= isset($data['email']) ? $data['email'] : '';
-			$staff->card_code= isset($data['card_code']) ? $data['card_code'] : '';
-			$staff->card_id= isset($data['card_id']) ? $data['card_id'] : '';
-			$staff->att_code= isset($data['att_code']) ? $data['att_code'] : '';
-			$staff->department_id= isset($data['department_id']) ? $data['department_id'] : '';
-			$staff->image= isset($data['image']) ? $data['image'] : '';
-			$staff->created_by= isset($data['created_by']) ? $data['created_by'] : Yii::$app->user->identity->id;
-			$staff->company_id= isset($data['company_id']) ? $data['company_id'] : '';
-			$staff->description= isset($data['description']) ? $data['description'] : '';
-			$staff->created_time= date("Y-m-d H:i:s");
-			$staff->order= isset($data['order']) ? $data['order'] : '';
-			$save = $staff->save(false);
+            $staff = new Staff();
+            $staff->name = isset($data['name']) ? $data['name'] : '';
+            $staff->phone = isset($data['phone']) ? $data['phone'] : '';
+            $staff->email = isset($data['email']) ? $data['email'] : '';
+            $staff->card_code = isset($data['card_code']) ? $data['card_code'] : '';
+            $staff->card_id = isset($data['card_id']) ? $data['card_id'] : '';
+            $staff->att_code = isset($data['att_code']) ? $data['att_code'] : '';
+            $staff->department_id = isset($data['department_id']) ? $data['department_id'] : '';
+            $staff->image = isset($data['image']) ? $data['image'] : '';
+            $staff->created_by = isset($data['created_by']) ? $data['created_by'] : Yii::$app->user->identity->id;
+            $staff->company_id = isset($data['company_id']) ? $data['company_id'] : '';
+            $staff->description = isset($data['description']) ? $data['description'] : '';
+            $staff->created_time = date("Y-m-d H:i:s");
+            $staff->order = isset($data['order']) ? $data['order'] : '';
+            $save = $staff->save(false);
             if ($save) {
-				if($staff->image){
-					//upload image 
-					$path = Yii::$app->params['images']['staff']['path'].'/'. $staff->company_id;
-					$option = ['width'=> 120,'height'=> 120];
-					\common\components\Common::uploadFile($staff->image,$path,$staff,'.png',$option,true);
-				}
+                if ($staff->image) {
+                    //upload image
+                    $path = Yii::$app->params['images']['staff']['path'] . '/' . $staff->company_id;
+                    $option = ['width' => 120, 'height' => 120];
+                    \common\components\Common::uploadFile($staff->image, $path, $staff, '.png', $option, true);
+                }
                 $return = array(
                     'error_code' => 0,
                     'message' => 'Success'
@@ -92,8 +93,8 @@ class StaffController extends Controller
      */
     public function actionInfo()
     {
-		$this->logger->LogInfo("actionInfo staff data  :" .json_encode(Yii::$app->request->get()));
-		
+        $this->logger->LogInfo("actionInfo staff data  :" . json_encode(Yii::$app->request->get()));
+
         if (Yii::$app->user->isGuest) {
             return ['error_code' => 1, 'message' => 'Not login'];
         }
@@ -104,7 +105,7 @@ class StaffController extends Controller
         $staff = Staff::find()->where(['and', ['id' => $id]])
             ->orWhere(['card_code' => $card_code])->one();
         if ($staff)
-            $staff->image = \common\components\Common::getImage($staff,'staff');
+            $staff->image = \common\components\Common::getImage($staff, 'staff');
         return ['error_code' => 0, 'message' => 'Success', 'data' => $staff];
 
     }
@@ -114,25 +115,17 @@ class StaffController extends Controller
      */
     public function actionGet()
     {
-        $this->logger->LogInfo("actionInfo staff data  :" .json_encode(Yii::$app->request->get()));
+        $this->logger->LogInfo("actionInfo staff data  :" . json_encode(Yii::$app->request->get()));
         if (Yii::$app->user->isGuest) {
             return ['error_code' => 1, 'message' => 'Not login'];
         }
         $staff = [];
         $message = '';
-        $id = isset(Yii::$app->request->get()['id']) ? Yii::$app->request->get()['id'] : '';
         $user_id = isset(Yii::$app->request->get()['user_id']) ? Yii::$app->request->get()['user_id'] : '';
-        if (!empty($id)) {
-            $staff = Staff::findOne(['id' => $id]);
-            if ($staff)
-                $staff->image = \common\components\Common::getImage($staff,'staff');
+        $company_id = isset(Yii::$app->request->get()['company_id']) ? Yii::$app->request->get()['company_id'] : '';
+        $staff = Staff::getStaff($user_id, $company_id);
+        if (!empty($staff)) {
             return ['error_code' => 0, 'message' => 'Success', 'data' => $staff];
-        } elseif (!empty($user_id)) {
-            $staff = Staff::getStaffByUserId(['user_id' => $user_id]);
-            if (!empty($staff)) {
-                return ['error_code' => 0, 'message' => 'Success', 'data' => $staff];
-            }
-
         }
         return ['error_code' => 401, 'message' => 'Data empty', 'data' => []];
     }
