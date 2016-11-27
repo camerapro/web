27f2d13c4fc95c506abc92a534b32844933f36b3
thongnv10 <thongnv1010@gmail.com>
2016-11-27 18:22:12 +0700

    update log

--- api/controllers/DepartmentController.php
+++ api/controllers/DepartmentController.php
@@ -46,5 +46,43 @@ class DepartmentController extends ApiController
 			return ['error_code' => 401, 'message' => 'Data empty', 'data' => []];
 		}
     }
-	
+    public function actionAdd()
+    {
+        $this->logger->LogInfo("actionAdd department data  :" . json_encode(Yii::$app->request->post()));
+
+        if (Yii::$app->user->isGuest) {
+            return ['error_code' => 1, 'message' => 'Not login'];
+        }
+        if ($data = Yii::$app->request->post()) {
+
+            $staff = new Department();
+            $staff->name = isset($data['name']) ? $data['name'] : '';
+            $staff->company_id = isset($data['company_id']) ? $data['company_id'] : '';
+            $staff->parent_id = isset($data['parent_id']) ? $data['parent_id'] : '';
+            $staff->local_department_id = isset($data['local_department_id']) ? $data['local_department_id'] : '';
+            $staff->created_time = date("Y-m-d H:i:s");
+            $staff->order = isset($data['order']) ? $data['order'] : '';
+            $save = $staff->save(false);
+
+            if ($save) {
+                $return = array(
+                    'error_code' => 0,
+                    'message' => 'Success'
+                );
+            } else {
+                $return = array(
+                    'error_code' => 1,
+                    'message' => 'Add fail'
+                );
+            }
+        } else {
+            $return = array(
+                'error_code' => 1,
+                'message' => 'Method not supported!'
+            );
+        }
+        echo json_encode($return);
+        exit;
+
+    }
 }
