ec16445d6bb657403f26fefd2956083aa12e5905
thongnv <thongnv@vega.com.vn>
2016-11-29 18:09:59 +0700

    Update

--- frontend/views/timekeeping/index.php
+++ frontend/views/timekeeping/index.php
@@ -56,7 +56,6 @@ $this->params['breadcrumbs'][] = $this->title;
 		var from_time = $("#from_time").val();
 		var to_time = $("#to_time").val();
 		var url = 'http://cam.thietbianninh.com/timekeeping/export?from_time='+from_time+'&to_time='+to_time;
-		alert(url);
 		var win = window.open(url, '_blank');
         return false;
 
