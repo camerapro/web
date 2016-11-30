8d2fb7423fc4cd6e80b7fb5560af2ca1f3160675
thongnv <thongnv@vega.com.vn>
2016-11-30 10:25:20 +0700

    Update

--- frontend/views/timekeeping/_search.php
+++ frontend/views/timekeeping/_search.php
@@ -47,7 +47,7 @@ use kartik\daterange\DateRangePicker;
 		timePicker24Hour: true,
         calender_style: "picker_4",
         locale: {
-            format: 'mm:HH DD-MM-YYYY'
+            format: 'HH:mm DD-MM-YYYY'
         },
     }, function(start, end, label) {
         console.log(start.toISOString(), end.toISOString(), label);
@@ -58,7 +58,7 @@ use kartik\daterange\DateRangePicker;
 		timePicker24Hour: true,
         calender_style: "picker_4",
         locale: {
-            format: 'mm:HH DD-MM-YYYY'
+            format: 'HH:mm DD-MM-YYYY'
         },
     }, function(start, end, label) {
         console.log(start.toISOString(), end.toISOString(), label);
