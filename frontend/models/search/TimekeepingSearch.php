2df15eb5b4c09d32093c2e69632fbfcae456be38
thongnv <thongnv@vega.com.vn>
2016-12-01 18:49:23 +0700

    Update TimekeepingSearch

--- frontend/models/search/TimekeepingSearch.php
+++ frontend/models/search/TimekeepingSearch.php
@@ -15,11 +15,13 @@ class TimekeepingSearch extends TimekeepingFrontend
     /**
      * @inheritdoc
      */
+	public $staff;
+	public $tat;
     public function rules()
     {
         return [
             [['id', 'status', 'tat_id', 'staff_id','deleted'], 'integer'],
-            [['card_code', 'staff_name', 'type', 'created_time', 'image'], 'safe'],
+            [['card_code', 'staff_name', 'type', 'created_time', 'image','staff','tat'], 'safe'],
         ];
     }
 
@@ -41,16 +43,15 @@ class TimekeepingSearch extends TimekeepingFrontend
      */
     public function search($params)
     {
-        $query = TimekeepingFrontend::find()->innerJoinWith('staff')->innerJoinWith('tat');
+        $query = TimekeepingFrontend::find()->innerJoinWith('staff')->innerJoinWith('tat')->orderBy(['created_time' => SORT_DESC]);;
         
         // add conditions that should always apply here
+		
 
+        $this->load($params);
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
         ]);
-
-        $this->load($params);
-
         if (!$this->validate()) {
             // uncomment the following line if you do not want to return any records when validation fails
             // $query->where('0=1');
@@ -58,32 +59,26 @@ class TimekeepingSearch extends TimekeepingFrontend
         }
 		$deleted = isset($params['deleted'])?$params['deleted']:0;
         // grid filtering conditions
-        $query->andFilterWhere([
-            'id' => $this->id,
-            'timekeeping.status' => $this->status,
-            'tat_id' => $this->tat_id,
-            'staff_id' => $this->staff_id,
-            'deleted' => $this->deleted,
-        ]);
-		
-        $query->andFilterWhere(['like', 'card_code', $this->card_code])
-            ->andFilterWhere(['like', 'staff_name', $this->staff_name])
-            ->andFilterWhere(['like', 'type', $this->type])
-            ->andFilterWhere(['like', 'deleted', $this->deleted])
-            ->andFilterWhere(['like', 'image', $this->image]);
 		
 		$dataProvider->query->where(['timekeeping.deleted'=>$deleted]);
+		
 		if($params['from_time']){
 			$dataProvider->query->andWhere(['>=', 'timekeeping.created_time', $params['from_time']]);
 		}
 		if($params['to_time']){
 			 $dataProvider->query->andWhere(['<=', 'timekeeping.created_time', $params['to_time']]);
 		}
-
 		 if (isset(Yii::$app->user->identity->level) && Yii::$app->user->identity->level < 4){
 			 $company_id = Yii::$app->user->identity->company_id;
 			 $dataProvider->query->andWhere(['=', 'timekeeping.company_id', $company_id]);
 		 }
+		$staff_name = explode(',',$this->staff_name);
+		$i= 0;
+		foreach($staff_name as $val){
+			$i++;
+			$dataProvider->query->andWhere(['like', 'staff.name', $val]);
+			break;
+		}
         return $dataProvider;
     }
 }
