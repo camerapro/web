3e140ea34bf1cbdad0653e2674a1b50b77ae7548
thongnv <thongnv@vega.com.vn>
2016-12-09 15:34:10 +0700

    update company/_form.php

--- frontend/views/company/_form.php
+++ frontend/views/company/_form.php
@@ -41,17 +41,40 @@ use yii\bootstrap\ActiveForm;
     <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'phone')->textInput(['maxlength' => true]) ?>
+<p>Thông tin tài khoản quản trị: </p>
+	<?php
+			$user = new \common\models\User();
+			
+	?> 
+   <div class="x_content">
+    <?= $form->field($user, 'username')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($user, 'password')->passwordInput() ?>
+    <?= $form->field($user, 'fullname')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($user, 'email')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($user, 'phone')->textInput(['maxlength' => true]) ?>
+	<div class="item form-group">
+		<label class="control-label col-md-3 col-sm-3 col-xs-12">Phân quyền</span>
+		</label>
+		<div class="col-md-6 col-sm-6 col-xs-12">
 
+			<select id="level" class="form-control" required  name="level">
+				<?php
+				$lever = \frontend\models\Level::find()->where(['<=', 'id', Yii::$app->user->identity->level])->andWhere(['=', 'status', 1])->all();
+				?>
+				<?php foreach ($lever as $item):?>
+					<option <?= ($user->level == $item->id) ? 'selected' : ''?> value="<?= $item->id?>"><?= $item->level_name?></option>
+				<?php endforeach;?>
+			</select>
+		</div>
 
-    <p>Thông tin quản trị:</p>
-    <?= $form->field($model, 'balance')->textInput(['maxlength' => true]) ?>
-
-     <?= $form->field($model, 'created_time')->textInput(array('value'=>isset($model->created_time)?$model->created_time:date("Y-m-d H:i:s"))) ?>
-
-    <?= $form->field($model, 'expired_time')->textInput(array('value'=>isset($model->expired_time)?$model->expired_time:date("Y-m-d H:i:s",time()+ 365*86400)));?>
-
-    <?= $form->field($model, 'website')->textInput(['maxlength' => true]) ?>
-
+	</div>
+	<p>Thông tin quản trị: </p>
+    <?= $form->field($user, 'address')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($user, 'created_time')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($user, 'expired_time')->textInput(['maxlength' => true]) ?>
+	<?= $form->field($model, "status")->checkbox(); ?>
+	
+	
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Lưu' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
