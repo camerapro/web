38c67f69539525326594d3f3cee19382381a3eb1
thongnv10 <thongnv1010@gmail.com>
2016-12-10 21:17:20 +0700

    update agency/_form.php

--- frontend/views/agency/_form.php
+++ frontend/views/agency/_form.php
@@ -56,143 +56,45 @@ use yii\bootstrap\ActiveForm;
     <div class="clearfix" style="padding-top: 10px;"></div>
     <p>Thông tin tài khoản quản trị:</p>
     <div class="pull-center" >
-        <?php
+		<?php
 			$user = new \common\models\User();
 			
-		?> 
-		 <div class="x_content">
-                <form class="form-horizontal form-label-left" method="get">
-                   <input type="hidden" name="id" value="<?= $user->id?>"/>
-                    <div class="item form-group">
-                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="name">Tên đăng nhập</label>
-                        <div class="col-md-6 col-sm-6 col-xs-12">
-                            <input id="name" class="form-control col-md-7 col-xs-12 has-feedback-left"  name="username" placeholder="Tên đăng nhập" required="required" type="text" value="<?= $user->username;?>">
-                            <span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
-                        </div>
-                    </div>
-                    <div class="item form-group">
-                        <label for="password" class="control-label col-md-3">Mật khẩu</label>
-                        <div class="col-md-6 col-sm-6 col-xs-12">
-                            <input id="password" type="password" name="password" data-validate-length="6,11" class="form-control col-md-7 col-xs-12 has-feedback-left" placeholder="Mật khẩu">
-                            <span class="fa fa-key form-control-feedback left" aria-hidden="true"></span>
-                        </div>
-                    </div>
-                    <div class="item form-group">
-                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="name">Tên đầy đủ</label>
-                        <div class="col-md-6 col-sm-6 col-xs-12">
-                            <input id="name" class="form-control col-md-7 col-xs-12 has-feedback-left" data-validate-length-range="6" data-validate-words="2" name="name" placeholder="Tên đầy đủ" type="text" value="<?= $user->fullname;?>">
-                            <span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
-                        </div>
-                    </div>
-                    <div class="item form-group">
-                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="name">Email</label>
-                        <div class="col-md-6 col-sm-6 col-xs-12">
-                            <input type="email" id="email" name="email" required="required" class="form-control col-md-7 col-xs-12 has-feedback-left" placeholder="Email" value="<?= $user->email;?>">
-                            <span class="fa fa-envelope form-control-feedback left" aria-hidden="true"></span>
-                        </div>
-                    </div>
-                    <div class="item form-group">
-                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="name">Số điện thoại</label>
+	?> 
+   <div class="x_content">
+    <?= $form->field($user, 'username')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($user, 'password')->passwordInput() ?>
+    <?= $form->field($user, 'fullname')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($user, 'email')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($user, 'phone')->textInput(['maxlength' => true]) ?>
+	<?= $form->field($user, 'address')->textInput(['maxlength' => true]) ?>
+	<div class="item form-group">
+		<label class="control-label col-md-3 col-sm-3 col-xs-12">Phân quyền</span>
+		</label>
+		<div class="col-md-6 col-sm-6 col-xs-12">
 
-                        <div class="col-md-6 col-sm-6 col-xs-12">
-                            <input type="tel" id="telephone" name="phone" required="required" data-validate-length-range="8,20" class="form-control col-md-7 col-xs-12 has-feedback-left" placeholder="Số điện thoại" value="<?= $user->phone;?>">
-                            <span class="fa fa-phone form-control-feedback left" aria-hidden="true"></span>
-                        </div>
-                    </div>
+			<select id="level" class="form-control" required  name="level">
+				<?php
+				$lever = \frontend\models\Level::find()->where(['<=', 'id', Yii::$app->user->identity->level])->andWhere(['=', 'status', 1])->all();
+				?>
+				<?php foreach ($lever as $item):?>
+					<option <?= ($user->level == $item->id) ? 'selected' : ''?> value="<?= $item->id?>"><?= $item->level_name?></option>
+				<?php endforeach;?>
+			</select>
+		</div>
 
-                    <div class="item form-group">
-                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="occupation">Đia chỉ</label>
-                        <div class="col-md-6 col-sm-6 col-xs-12">
-                            <input id="occupation" type="text" name="occupation" data-validate-length-range="5,200" class="optional form-control col-md-7 col-xs-12 has-feedback-left" placeholder="Đia chỉ" value="<?= $user->address;?>">
-                            <span class="fa fa-map-marker form-control-feedback left" aria-hidden="true"></span>
-
-                        </div>
-                    </div>
-                    <div class="item form-group">
-                        <label class="control-label col-md-3 col-sm-3 col-xs-12">Phân quyền</span>
-                        </label>
-                        <div class="col-md-6 col-sm-6 col-xs-12">
-
-                            <select id="level" class="form-control" required  name="level">
-                                <?php
-                                $lever = \frontend\models\Level::find()->where(['<=', 'id', Yii::$app->user->identity->level])->andWhere(['=', 'status', 1])->all();
-                                ?>
-                                <?php foreach ($lever as $item):?>
-                                    <option <?= ($user->level == $item->id) ? 'selected' : ''?> value="<?= $item->id?>"><?= $item->level_name?></option>
-                                <?php endforeach;?>
-                            </select>
-                        </div>
-
-                    </div>
-
-                    <div class="item form-group">
-                        <label class="control-label col-md-3 col-sm-3 col-xs-12">Nhóm quyền</span>
-                        </label>
-                        <div class="col-md-3 col-sm-3 col-xs-12">
-                            <select id="permission" class="form-control" required  name="permission">
-                                <?php
-                                $lever = \frontend\models\PermissionGroup::findAll(['status'=>1]);
-                                if(Yii::$app->user->identity->level <4){
-//                                    $lever = \frontend\models\PermissionGroup::findAll(['id'=>Yii::$app->user->identity->permission_group_id]);
-                                    $lever = \frontend\models\PermissionGroup::find()->orWhere(['id'=>Yii::$app->user->identity->permission_group_id])->orWhere(['created_by_id'=>Yii::$app->user->identity->id])->all();
-                                }
-                                ?>
-                                <?php foreach ($lever as $item):?>
-                                    <option <?= ($item->id == $user->permission_group_id) ? 'selected' : ''?> value="<?= $item->id?>"><?= $item->name?></option>
-                                <?php endforeach;?>
-                            </select>
-                        </div>
-                        <?php if(Yii::$app->user->identity->level >=3): ?>
-                        <div class="col-md-3 col-sm-3 col-xs-12">
-                            <a data-ignore-state="1" id="notify-id"  data-target="#show_create_per" data-toggle="modal" class="title pull-left btn btn-success" href="/ajax/show_create_per">Tạo nhóm quyền</a>
-                        </div>
-                        <?php endif;?>
-                    </div>
-
-                    <div class="item form-group">
-                        <label class="control-label col-md-3 col-sm-3 col-xs-12">Công ty</span>
-                        </label>
-                        <div class="col-md-6 col-sm-6 col-xs-12">
-                            <select id="heard" class="form-control" required  name="company_id">
-                                <?php
-                                $company = \frontend\models\FrontendCompany::findAll(['status'=>1]);
-                                if(Yii::$app->user->identity->level <4){
-                                    $company = \frontend\models\FrontendCompany::findAll(['id'=>Yii::$app->user->identity->company_id, 'status'=>1]);
-                                }
-                                ?>
-                                <?php foreach ($company as $item):?>
-                                    <option <?= ($item->id == $user->company_id) ? 'selected' : ''?> value="<?= $item->id?>"><?= $item->name?></option>
-                                <?php endforeach;?>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="item form-group">
-                        <label class="control-label col-md-3 col-sm-3 col-xs-12">Thời gian hết hạn</span>
-                        </label>
-                        <div class="col-md-6 col-sm-6 col-xs-12">
-                            <input id="expired_time" name="expired_time" class="date-picker form-control col-md-7 col-xs-12" required="required" type="text" value="<?= isset($user->expired_time) ? date('d-m-Y', strtotime($user->expired_time)) :date('d-m-Y' , strtotime('+3 months', time())) ;?>">
-                        </div>
-                    </div>
-
-                    <div class="item form-group">
-                        <label class="control-label col-md-3 col-sm-3 col-xs-12">Khóa</span>
-                        </label>
-                        <div class="col-md-6 col-sm-6 col-xs-12">
-                            <input tabindex="1" type="checkbox" id="lock_user" name="lock_user" <?php echo ($model->status == 0) ? 'checked="checked"' : ''?>/>
-                        </div>
-                    </div>
-
-
-                    <div class="ln_solid"></div>
-                    <div class="form-group">
-                        <div class="col-md-6 col-md-offset-3">
-                            <button type="cancel" onclick="javascript:window.location='<?=\yii\helpers\Url::base()?>/user/index';" class="btn btn-primary">&nbsp;&nbsp;&nbsp;&nbsp;  Hủy&nbsp;&nbsp; &nbsp;&nbsp; </button>
-                            <button id="send" type="submit" class="btn btn-success"><?= $model->isNewRecord ? 'Lưu' : 'Chỉnh sửa'?></button>
-                        </div>
-                    </div>
-                </form>
-            </div>
+	</div>
+	<p>Thông tin quản trị: </p>
+   
+    <?= $form->field($user, 'created_time')->textInput(['maxlength' => true,'value'=>date('d-m-Y',time())]) ?>
+    <?= $form->field($user, 'expired_time')->textInput(['maxlength' => true,'value'=>date('d-m-Y',time()+365*86400)]) ?>
+	<?= $form->field($user, "status")->checkbox()->label('Khóa'); ?>
+	
+	
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Lưu' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
+    </div>
+		
 
     <?php ActiveForm::end(); ?>
 </div>
