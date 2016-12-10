7dc0036e64b344ff0a6d0b39cc85bf4190fcf0d7
thongnv10 <thongnv1010@gmail.com>
2016-12-10 20:56:16 +0700

    update

--- frontend/controllers/CompanyController.php
+++ frontend/controllers/CompanyController.php
@@ -8,6 +8,7 @@ use frontend\models\search\CompanySearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use frontend\models\FrontendUser;
 
 /**
  * CompanyController implements the CRUD actions for CompanyFrontend model.
@@ -72,7 +73,39 @@ class CompanyController extends Controller
         $model = new CompanyFrontend();
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect("/company/index");
+			//create user 
+			$data = Yii::$app->request->post();
+			$user_data = $data['User'];
+			$user_name = $user_data['username'];
+            $user = FrontendUser::findOne(['username'=>$user_name]);
+            if(!$user){
+                 $user = new FrontendUser();
+            }
+            $user->username = $user_data['username'];
+            $user->fullname = $user_data['fullname'];
+            $user->email = $user_data['email'];
+            $user->phone = $user_data['phone'];
+      
+            $user->password = md5($user_data['password']);
+            $user->address = $user_data['address'];
+            $user->level = $data['level'];
+     
+            $user->expired_time = date('Y-m-d', strtotime($user_data['expired_time']));
+            $user->created_time = date('Y-m-d H:i:s');
+            $user->updated_time = date('Y-m-d H:i:s');
+            $user->status = 1;
+            $user->company_id = $model->id;
+            if(isset($user_data['status']) && $user_data['status'] == 1){
+                $user->status = 0;
+            }
+            $user->permission_group_id =  isset($data['permission']) ? $data['permission'] : 1;
+            if ($user->save(false)) {
+                 return $this->redirect("/company/index");
+            }else{
+                $error = 'Có lỗi xảy ra, vui lòng liên hệ kỹ thuật';
+                return;
+            }
+           
         } else {
 
             return $this->render('create', [
