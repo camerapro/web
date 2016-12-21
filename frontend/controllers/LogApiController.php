41cf59118981af6315853c1343e40d7574a47d48
thongnv <thongnv@vega.com.vn>
2016-12-21 15:01:57 +0700

    Update log API

---
+++ frontend/controllers/LogApiController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace frontend\controllers;
+
+use common\components\FrontendController;
+use Yii;
+use frontend\models\StaffFrontend;
+use frontend\models\search\StaffSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use yii\web\UploadedFile;
+
+/**
+ * StaffController implements the CRUD actions for StaffFrontend model.
+ */
+class LogApiController extends FrontendController
+{
+    public $enableCsrfValidation = false;
+	
+	
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Lists all StaffFrontend models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $log_path = '/srv/www/cmr/runtime/';
+		$date = isset(Yii::$app->request->get()['data']) ? Yii::$app->request->get()['date'] : date("Ymd");
+		$file_name = $log_path.'api_'.$date;
+        return $this->render('index', [
+            'file_name' => $file_name
+        ]);
+    }
+
+    
+}
---
+++ frontend/views/log-api/index.php
@@ -0,0 +1,20 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel frontend\models\search\UserLogSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Log API';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="user-log-index">
+
+    <h4><?= Html::encode($this->title) ?></h4>
+  <div style="max-height: 800px;overflow: auto; background: #000;color: #fff;;">
+		
+  </div>
+   
+</div>
